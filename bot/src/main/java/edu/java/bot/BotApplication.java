4f363acdcea4d6a443255edbad2121afbb977a68
Daniil <Danmarchenko2004@yandex.ru>
2024-02-23 00:19:25 +0300

    исправлен checkstyle

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -4,7 +4,6 @@ import edu.java.bot.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
