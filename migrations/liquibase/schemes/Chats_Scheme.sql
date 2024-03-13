0485692121c9658173b6c43b2a154aaa3b9cee13
Daniil <Danmarchenko2004@yandex.ru>
2024-03-13 15:22:37 +0300

    правки ДЗ №4

--- migrations/liquibase/schemes/Chats_Scheme.sql
+++ migrations/liquibase/schemes/Chats_Scheme.sql
@@ -3,5 +3,5 @@ create table Chats
     chat_id bigint,
 
     primary key (chat_id),
-    unique (chat_id)
+    created_at timestamp with time zone not null
 )
--- scrapper/src/test/java/edu/java/scrapper/db/DataBaseIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/db/DataBaseIntegrationTest.java
@@ -9,5 +9,17 @@ public class DataBaseIntegrationTest extends IntegrationEnvironment {
     @Test
     public void dataBaseTest() {
         assertThat(liquibase.getDatabase()).isNotNull();
+
+        String query = "SELECT COUNT(*) FROM information_schema.tables WHERE " +
+            "table_name = ?";
+
+        assertThat(template.queryForObject(query, Integer.class, "chats"))
+            .isEqualTo(1);
+
+        assertThat(template.queryForObject(query, Integer.class, "links"))
+            .isEqualTo(1);
+
+        assertThat(template.queryForObject(query, Integer.class, "chat_link_connection"))
+            .isEqualTo(1);
     }
 }
